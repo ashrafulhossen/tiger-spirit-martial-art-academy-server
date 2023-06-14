253ae9e0b4216a4eda5508aeea4486a153a8672a
ashrafulhossen <b.asrafulhossencht@gmail.com>
2023-06-14 23:04:00 +0600

    add deny class feature

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Essential Setup
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+const corsConfig = {
+	origin: "",
+	credentials: true,
+	methods: ["GET", "POST", "PUT", "DELETE"]
+};
 app.use(cors());
+app.options("", cors(corsConfig));
 app.use(express.json());
 
 // express server
@@ -27,17 +34,84 @@ const client = new MongoClient(uri, {
 	}
 });
 
+const userCollection = client
+	.db("tigerSpiritMartialArtAcademyDB")
+	.collection("user");
 const instructorCollection = client
 	.db("tigerSpiritMartialArtAcademyDB")
 	.collection("instructor");
 const classCollection = client
 	.db("tigerSpiritMartialArtAcademyDB")
 	.collection("class");
+const selectedClassCollection = client
+	.db("tigerSpiritMartialArtAcademyDB")
+	.collection("selectedClass");
+
+// create class index based on enrollment
+classCollection.createIndex({ enroll: 1 });
+
+// JSON WEB TOKEN
+const JWT_SECRECT = process.env.JWT_SECRECT;
+const verifingJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+
+	if (!authorization) {
+		return res
+			.status(401)
+			.send({ error: true, message: "unauthorized access" });
+	}
+	const token = authorization.split(" ")[1];
+	jwt.verify(token, JWT_SECRECT, (err, decoded) => {
+		if (err) {
+			return res
+				.status(403)
+				.send({ error: true, message: "unauthorized access" });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
 
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		// await client.connect();
+
+		// JWT
+		app.post("/jwt", (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, JWT_SECRECT, { expiresIn: "1h" });
+			res.send({ token });
+		});
+
+		// get all users
+		app.get("/users", async (req, res) => {
+			const result = await userCollection.find().toArray();
+			res.send(result);
+		});
+
+		// get single user
+		app.get("/users/:uid", async (req, res) => {
+			const userUid = req.params.uid;
+			const query = { uid: userUid };
+			const result = await userCollection.findOne(query);
+			res.send(result);
+		});
+
+		// post user
+		app.post("/users", async (req, res) => {
+			const user = req.body;
+			const isAnyUser = await userCollection.find().toArray();
+			if (isAnyUser.length == 0) {
+				user.role = "admin";
+			}
+			const result = await userCollection.insertOne(user);
+			res.send(result);
+		});
+
+		
+
+		
 
 		// get all instructors
 		app.get("/instructors", async (req, res) => {
@@ -45,24 +119,67 @@ async function run() {
 			res.send(result);
 		});
 
-        // get popular instructors
-        app.get("/popularInstructors", async (req, res) => {
-            const result = await instructorCollection.find().limit(6).toArray();
-            res.send(result);
-        })
+		// get popular instructors
+		app.get("/popularInstructors", async (req, res) => {
+			const result = await instructorCollection.find().limit(6).toArray();
+			res.send(result);
+		});
+
+		
 
 		// get all approved classes
 		app.get("/classes", async (req, res) => {
 			const query = { approved: true };
-			const result = await classCollection.find().toArray();
+			const result = await classCollection.find(query).toArray();
 			res.send(result);
 		});
 
-        // get popular classes
-        app.get("/popularClasses", async (req, res) => {
-            const result = await classCollection.find().limit(15).toArray();
-            res.send(result);
-        })
+		// get single class
+		app.get("/classes/:classId", async (req, res) => {
+			const classId = req.params.classId;
+			console.log(classId);
+			const filter = { _id: new ObjectId(classId) };
+			const result = await classCollection.findOne(filter);
+			res.send(result);
+		});
+
+		// post class
+		app.post("/class", async (req, res) => {
+			const classObj = req.body;
+			const result = await classCollection.insertOne(classObj);
+			res.send(result);
+		});
+
+		// get popular classes
+		app.get("/popularClasses", async (req, res) => {
+			const result = await classCollection.find().limit(10).toArray();
+			res.send(result);
+		});
+
+		
+
+		
+
+		
+
+		
+		
+
+		
+
+		// deny class
+		app.put("/selectedClass/:classId/denied", async (req, res) => {
+			const classId = req.params.classId;
+			const filter = { _id: new ObjectId(classId) };
+			const update = {
+				$set: {
+					status: "denied"
+				}
+			};
+			const result = await classCollection.updateOne(filter, update);
+			console.log(result);
+			res.send(result);
+		});
 
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
